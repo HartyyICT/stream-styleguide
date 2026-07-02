561da7d0fe4f059b7e4a5c611cf7ba49ded5e359
HartyyICT <130041604+HartyyICT@users.noreply.github.com>
2026-07-02 16:37:53 +0200

    STRUCTURAL-REVIEW.md Toegepast

--- src/app/components/atoms/CardTitle.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Typography, type TypographyProps } from "@mui/material";
-
-export default function CardTitle(props: TypographyProps) {
-  return <Typography {...props} variant="h3" />;
-}
--- src/app/components/organisms/SearchExperience.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import SearchDialog from "@/app/components/molecules/SearchDialog";
-
-export default function SearchExperience() {
-  return <SearchDialog />;
-}
